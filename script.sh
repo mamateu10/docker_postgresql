f581287d5e63bb27dab849db0b94b23095417d41
Marta Mateu <104842668+mamateu10@users.noreply.github.com>
2025-03-04 01:34:05 +0100

    1

--- script.sh
+++ script.sh
@@ -6,14 +6,25 @@ set -e
 # iniciem el servei de postgresql
 service postgresql start
 
+sleep 5
+
 # canviem la contrasenya de l'usuari postgres
 psql -U postgres -c "ALTER USER postgres WITH PASSWORD '${POSTGRES_DEFAULT_PASS}';" 
+
+echo "Contrasenya de l'usuari postgres canviada";
+
 # creem la base de dades
-psql -U postgres -c "CREATE DATABASE videogames;" && \
+psql -U postgres -c "CREATE DATABASE videogames;"
+
 # importam la base de dades
-psql -U postgres -d videogames -f /home/backup/backup_videogames.sql && \
+psql -U postgres -d videogames -f /home/backup/backup_videogames.sql
+
 # crear usuari de postgresql
-psql -U postgres -c "CREATE USER \"${POSTGRES_USER}\" WITH PASSWORD '${POSTGRES_PASSWORD}';" && \
+psql -U postgres -c "CREATE USER \"${POSTGRES_USER}\" WITH PASSWORD '${POSTGRES_PASSWORD}';"
+
 # donam permisos a la base de dades
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE videogames TO \"${POSTGRES_USER}\";"
 
+echo "✅ PostgreSQL configurat correctament!"
+
+service postgresql stop
