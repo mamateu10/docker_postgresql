43a32cd55ed13180227fd4adb3460da302b37022
Marta Mateu <104842668+mamateu10@users.noreply.github.com>
2025-03-04 01:04:17 +0100

    prova 1

--- script.sh
+++ script.sh
@@ -13,3 +13,12 @@ psql -U postgres -d videogames -f /home/backup/backup_videogames.sql && \
 psql -U postgres -c "CREATE USER \"${POSTGRES_USER}\" WITH PASSWORD '${POSTGRES_PASSWORD}';" && \
 # donam permisos a la base de dades
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE videogames TO \"${POSTGRES_USER}\";"
+
+
+
+# descarregem el script per a la configuració de postgresql i la creació de la base de dades
+RUN wget https://raw.githubusercontent.com/mamateu10/docker_postgresql/refs/heads/main/script.sh
+# assignar permisos al fitxer
+RUN chmod +x script.sh
+# executam el script
+RUN ./script.sh
